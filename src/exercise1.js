e4be55e9bb1ddab1bc74ccdd78087ba069c0f993
louloulfx <louloulfx1@hotmail.fr>
2018-03-13 10:09:53 +0100

    Exercise 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,5 +1,13 @@
 var removeNegativeValue = function(values) {
   // Your future job begins here ...
+    var output= []
+    return output;
+
+
+    var input = [34, -5, 9, 18, 0, -7, 32];
+    const result = input.filter(input => input > -1);
+
+    return result;
 }
 
 module.exports = {
