a02e4ee1c74f01327622548391d1ca0e776eb955
louloulfx <louloulfx1@hotmail.fr>
2018-03-13 10:31:54 +0100

    Exercice 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,5 +1,13 @@
 var applyMap = function() {
   // Your future job begins here ...
+
+  if (input === undefined)
+  {
+    var input;
+    var output = [];
+    return output;
+  }
+  
 }
 
 module.exports = {
