cc3731b83843680237782df6a5f0776c3181c41f
louloulfx <louloulfx1@hotmail.fr>
2018-03-13 10:43:04 +0100

    Exercice 2

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,6 +1,32 @@
-var mapCustomer = function(customer) {
-  // Your future job begins here ...
+var mapCustomer = function(input) {
+  if (input != {}) {
+    var output = Object.assign({}, input);
+    var somme = 0;
+    for (var i = 0; i < output.products.length; i++) {
+      somme += output.products[i].price;
+    }
+    output.moneySpent = Number(somme);
+    return output;
+
+  }
+  else {
+    var output = {
+      firstname: 'John',
+      lastname: 'Doe',
+      products: [],
+      moneySpent: 0
+    };
+    return output;
+
+  }
 }
+var input = {
+  firstname: 'Jack',
+  lastname: 'Smith',
+  products: [{ label: 'shoes', price: 89 }, { label: 'book', price: 14 }, { label: 'smartphone', price: 499 }]
+}
+
+
 
 module.exports = {
   title: 'Exercise 2',
